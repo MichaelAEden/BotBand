d0a6014beb596d57866aa107ac9b52e0dc031611
Jenny Xia <jennyxia@Jenny-Xias-MacBook-Pro.local>
2020-06-27 17:16:20 -0400

    Add tone.js and playMelody

--- client/src/Components/TopPanel.js
+++ client/src/Components/TopPanel.js
@@ -1,33 +1,46 @@
 import React, { Component } from 'react';
 import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
 import './styles/TopPanel.css'
+import Tone from 'tone';
 
 class TopPanel extends Component {
-    constructor() {
-        super();
-        this.state = {}
-        this.handleRobotClick = this.handleRobotClick.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = {}
+    this.handleRobotClick = this.handleRobotClick.bind(this);
+  }
 
-    handleRobotClick(e) {
-        console.log("hello");
-    }
+  playMelody(m) {
+    let synth = new Tone.Synth().toMaster();
+    let sequence = new Tone.Sequence(function(time, note){
+      synth.triggerAttackRelease(note, "4n", time);
+    }, m, "4n");
+    sequence.start();
+    sequence.loop = 1;
+    Tone.Transport.toggle();
+  }
 
-    render() {
-        return(
-            <div id="top-panel">
-                <MDBRow>
-                    <MDBCol size="3">
-                        <img src="Robot1.png" className="robot" onClick={this.handleRobotClick}></img>
-                    </MDBCol>
+  handleRobotClick(e) {
+    console.log("hello");
+    let m = ["C4", "E4", "G4", "A4"]
+    this.playMelody(m);
+  }
 
-                    <MDBCol size="3">
-                        <img src="Robot1.png" className="robot" onClick={this.handleRobotClick}></img>
-                    </MDBCol>
-                </MDBRow>
-            </div>
-        );
-    }
+  render() {
+    return(
+      <div id="top-panel">
+      <MDBRow>
+          <MDBCol size="3">
+          <img src="Robot1.png" className="robot" onClick={this.handleRobotClick}></img>
+          </MDBCol>
+
+          <MDBCol size="3">
+          <img src="Robot1.png" className="robot" onClick={this.handleRobotClick}></img>
+          </MDBCol>
+      </MDBRow>
+      </div>
+    );
+  }
 }
 
 export default TopPanel;
