4dde2ea5e9588eda322ef5ddf8930d7797c09adb
Mickey Dang <mickeydang7299@gmail.com>
2020-06-27 19:05:15 -0400

    added incrementing logic to models

--- server/models/Note.ts
+++ server/models/Note.ts
@@ -1,22 +1,69 @@
-export class Note {
-    key : String;
+import { notDeepEqual } from "assert";
 
-    constructor(key : String) {
-        this.validate(key);
-        this.key = key;
+export class Note {
+    note : String;
+
+    constructor(note : String) {
+        this.validate(note);
+        this.note = note;
     }
 
-    validate(key: String) {
-        if (key.length !== 2) {
+    validate(note: String) {
+        if (note.length !== 2) {
             throw new Error("Validation Error: Note malformed");
         }
     }
 
     getOctave(): String {
-        return this.key.charAt(1);
+        return this.note.charAt(1);
     }
 
     getKey(): String {
-        return this.key.charAt(0);
+        return this.note.charAt(0);
+    }
+
+    getNote(): String {
+        return this.note;
+    }
+
+    // this note - arg note
+    compare(note: Note): Number {
+        let dOctave = (Number(this.getOctave()) - Number(note.getOctave())) * 8;
+        let dNote = this.getKey().charCodeAt(0) - note.getKey().charCodeAt(0);
+
+        return dOctave + dNote;
+    }
+
+    increment(change: number): Note {
+
+        if (change > 7) {
+            throw Error("Number is too high!");
+        }
+
+        let octave = Number(this.getOctave());
+        let letter;
+
+        let nUnicode = this.getKey().charCodeAt(0) + change;
+    
+        if (nUnicode <= 'G'.charCodeAt(0) && nUnicode >= 'A'.charCodeAt(0)) {
+
+            letter = String.fromCharCode(nUnicode);
+
+        } else if (nUnicode > 'G'.charCodeAt(0)) {
+
+            // -1 is needed just from trial and error
+            letter = String.fromCharCode('A'.charCodeAt(0) + nUnicode - 'G'.charCodeAt(0) - 1);
+            octave++;
+
+        } else {
+
+            // +1 is needed just from trial and error
+            letter = String.fromCharCode('G'.charCodeAt(0) - ('A'.charCodeAt(0) - nUnicode) + 1);
+            octave--;
+
+        }
+
+        let s = String(letter) + String(octave)
+        return new Note(s);
     }
 }
--- server/models/RulesImpl.ts
+++ server/models/RulesImpl.ts
@@ -1,9 +1,19 @@
 import {Rule} from './Rule'
 import {Melody} from './Melody'
 
-class LeapRule extends Rule {
+export class LeapRule extends Rule {
 
-    apply(index: Number, changeSet: Set<String>, melody: Melody): Array<String> {
+    apply(index: Number, changeSet: Array<String>, melody: Melody): Array<String> {
+        
+        if (index < 2) {
+            return changeSet;
+        } else {
+
+            // Make sure it's not 2 leaps (delta is same)
+            // Make sure the previous thing is not a recovery (delta is different)
+
+        }
+
         return new Array<String>();
     }
 
