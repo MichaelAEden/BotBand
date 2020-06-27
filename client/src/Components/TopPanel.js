a11740888ac3da39437c7f39837070232b5d4c58
Michael Eden <Mikey.Eden@gmail.com>
2020-06-27 17:00:09 -0400

    Add mock data for melodies

--- client/src/Components/TopPanel.js
+++ client/src/Components/TopPanel.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
+import { MDBRow, MDBCol } from 'mdbreact';
+import { fetchJson } from '../Utils/request'
+
 import './styles/TopPanel.css'
 
 class TopPanel extends Component {
@@ -9,24 +11,48 @@ class TopPanel extends Component {
         this.handleRobotClick = this.handleRobotClick.bind(this);
     }
 
-    handleRobotClick(e) {
-        console.log("hello");
+    async componentDidMount() {
+      // const response = await fetchJson('/createbots/rating')
+      // if (response.data) this.setState({ bots: response.bots })
+      const mockData = {
+        bots: [
+          {
+            melody: [
+              { note: 'A4' },
+              { note: 'B4' },
+              { note: 'C4' }
+            ]
+          },
+          {
+             melody: [
+               { note: 'A4' },
+               { note: 'B4' },
+               { note: 'C4' }
+             ]
+          },
+        ]
+      }
+      this.setState({bots: mockData.bots})
+    }
+
+    handleRobotClick(i) {
+      console.log(i);
     }
 
     render() {
-        return(
-            <div id="top-panel">
-                <MDBRow>
-                    <MDBCol size="3">
-                        <img src="Robot1.png" className="robot" onClick={this.handleRobotClick}></img>
-                    </MDBCol>
+      if (!this.state.bots) return null;
 
-                    <MDBCol size="3">
-                        <img src="Robot1.png" className="robot" onClick={this.handleRobotClick}></img>
-                    </MDBCol>
-                </MDBRow>
-            </div>
-        );
+      return(
+        <div id="top-panel">
+          <MDBRow>
+            <MDBCol size="1"></MDBCol>
+              {this.state.bots.map((bot, i) => (<MDBCol key={i} size="3">
+                  <img src="Robot1.png" className="robot" onClick={() => this.handleRobotClick(i)}></img>
+                </MDBCol>)
+              )}
+          </MDBRow>
+        </div>
+      );
     }
 }
 
