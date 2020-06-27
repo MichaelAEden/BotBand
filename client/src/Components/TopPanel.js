860de9e617a3f60154aa9e6e82fcc5a0d02e07de
Jenny Xia <jennyxia@Jenny-Xias-MacBook-Pro.local>
2020-06-27 17:53:37 -0400

    Fix merge conflicts

--- client/src/Components/TopPanel.js
+++ client/src/Components/TopPanel.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
-import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
+import { MDBRow, MDBCol } from 'mdbreact';
+import { fetchJson } from '../Utils/request'
 import './styles/TopPanel.css'
 import Tone from 'tone';
-import { fetchJson } from '../Utils/request'
 
 class TopPanel extends Component {
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      bots: []
+    };
     this.handleRobotClick = this.handleRobotClick.bind(this);
   }
 
   async componentDidMount() {
     const response = await fetchJson('/createbots/rating', {method: 'POST'});
     console.log(response);
-    if (response.data) this.setState({ bots: response.bots })
+    if (response.data) this.setState({ bots: response.data.bots })
   }
 
   playMelody(m) {
@@ -34,17 +36,21 @@ class TopPanel extends Component {
   }
 
   render() {
+    console.log(this.state.bots);
+    if (!this.state.bots) return null;
+    
+    const bots = this.state.bots.map((bot, i) => (
+      <MDBCol key={i} size="3">
+        <img src="Robot1.png" className="robot" onClick={() => this.handleRobotClick(i)}></img>
+      </MDBCol>
+    ));
+
     return(
       <div id="top-panel">
-      <MDBRow>
-          <MDBCol size="3">
-          <img src="Robot1.png" className="robot" onClick={this.handleRobotClick}></img>
-          </MDBCol>
-
-          <MDBCol size="3">
-          <img src="Robot1.png" className="robot" onClick={this.handleRobotClick}></img>
-          </MDBCol>
-      </MDBRow>
+        <MDBRow>
+          <MDBCol size="1"></MDBCol>
+            {bots}
+        </MDBRow>
       </div>
     );
   }
