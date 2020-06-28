9bcaac5cc0cb8ccf0988f7eaba8f3fff9f7e7684
Jackson Zheng <j75zheng@edu.uwaterloo.ca>
2020-06-27 23:48:32 -0400

    deleted stuff

--- server/models/Rule.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Rule = void 0;
-var Rule = /** @class */ (function () {
-    function Rule() {
-    }
-    return Rule;
-}());
-exports.Rule = Rule;
