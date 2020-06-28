48869a51aaddf9b282e53b2176e31ed38a421173
unknown <tiatuinstra@gmail.com>
2020-06-27 23:39:05 -0400

    fixed variable name

--- server/app/FitnessConvention.ts
+++ server/app/FitnessConvention.ts
@@ -13,8 +13,8 @@ export const evaluate = (bots: Bot[]) => {
     let min_octave
     let max_octave
     let octave = new Array<String>()
-    for(let i = 0; i<bots[i].melody.notes.length; i++){
-      octave.push(bots[i].melody.notes[i].getOctave())
+    for(let i = 0; i<bots[j].melody.notes.length; i++){
+      octave.push(bots[j].melody.notes[i].getOctave())
       if(i==0){
         min_octave = octave[i]
           max_octave = octave[i]
@@ -35,25 +35,25 @@ export const evaluate = (bots: Bot[]) => {
     // Convention 2: Mostly stepwise motion (few leaps)
     let step_fitness = 0
     let note_diff
-    for(let i = 0; i<bots[i].melody.notes.length-1; i++){
-      note_diff.push(bots[i].melody.notes[i].compare(bots[i].melody.notes[i+1]))
+    for(let i = 0; i<bots[j].melody.notes.length-1; i++){
+      note_diff.push(bots[j].melody.notes[i].compare(bots[j].melody.notes[i+1]))
       if(Math.abs(note_diff[i]) <= 1){
         step_fitness+=1
       }
     }
-    if(step_fitness > bots[i].melody.notes.length*0.75){
+    if(step_fitness > bots[j].melody.notes.length*0.75){
       step_fitness = 1
     }else{
-      step_fitness = step_fitness/bots[i].melody.notes.length
+      step_fitness = step_fitness/bots[j].melody.notes.length
     }
     
     // Bonus points: "millenial whoop" (alternating between fifth and third notes in a major scale)
     let whoop_fitness = 0
     let mill_whoop = ['G','E','G','E','G','E','G','E']
-    for(let i = 0; i<bots[i].melody.notes.length; i++){
-      if(mill_whoop[i] == bots[i].melody.notes[i].getKey()){
+    for(let i = 0; i<bots[j].melody.notes.length; i++){
+      if(mill_whoop[i] == bots[j].melody.notes[i].getKey()){
         if (i > 0){
-          if((octave[i] == octave[i-1]) && (bots[i].melody.notes[i-1].getKey() == mill_whoop[i-1])){
+          if((octave[i] == octave[i-1]) && (bots[j].melody.notes[i-1].getKey() == mill_whoop[i-1])){
             whoop_fitness+=1
           }
         }else{
@@ -61,7 +61,7 @@ export const evaluate = (bots: Bot[]) => {
         }
       }
     }
-    whoop_fitness = (2**whoop_fitness)/(2**bots[i].melody.notes.length)
+    whoop_fitness = (2**whoop_fitness)/(2**bots[j].melody.notes.length)
 
     // Determine total fitness according to musical rules
     musical_fitness = ((octave_fitness + step_fitness)/2) + whoop_fitness
