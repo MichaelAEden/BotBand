ce90bacb510202b3b60be78da3bf4316b2a092df
Jackson Zheng <j75zheng@edu.uwaterloo.ca>
2020-06-27 22:58:19 -0400

    finished bottom panel

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import { MDBContainer } from "mdbreact";
-import { render } from '@testing-library/react';
-import TopPanel from './Components/TopPanel';
 import { fetchJson } from './Utils/request';
+import TopPanel from './Components/TopPanel';
+import BottomPanel from './Components/BottomPanel';
+import Tone from 'tone';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      bots: []
+      bots: [],
+      composition: []
     }
+    this.handleRobotClick = this.handleRobotClick.bind(this);
   }
 
   async componentDidMount() {
@@ -18,10 +21,37 @@ class App extends Component {
     if (response.data) this.setState({ bots: response.data.bots })
   }
 
+  playMelody(m) {
+    Tone.Transport.clear();
+    const synth = new Tone.Synth().toMaster();
+    const sequence = new Tone.Sequence(function(time, note){
+      synth.triggerAttackRelease(note, "4n", time);
+    }, m, "4n");
+    sequence.start(Tone.Transport.time);
+    sequence.loop = false;
+    Tone.Transport.start();
+  }
+
+  handleRobotClick(i) {
+    console.log(this.state.bots);
+    let new_composition = this.state.composition.slice();
+    const clicked_melody = this.state.bots[i];
+    new_composition.push(clicked_melody);
+    this.setState({composition: new_composition});
+  }
+
   render() {
     return(
       <MDBContainer id="App">
-        <TopPanel bots={this.state.bots}/>
+        <TopPanel
+          bots={this.state.bots}
+          handleRobotClick={this.handleRobotClick}
+          playMelody={this.playMelody}
+        />
+        <BottomPanel
+          composition={this.state.composition}
+          playMelody={this.playMelody}
+        />
       </MDBContainer>
     );
   }
---
+++ client/src/Components/BottomPanel.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { MDBContainer, MDBRow, MDBCol, MDBIcon } from "mdbreact";
+import './styles/BottomPanel.css';
+
+class BottomPanel extends Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+    this.handlePlayComposition = this.handlePlayComposition.bind(this);
+  }
+
+  handlePlayComposition(i) {
+    let melodies = [];
+    this.props.composition.forEach( (bot) => {
+      let melody = bot.melody.map(note => note.key);
+      melodies = melodies.concat(melody);
+    });
+    this.props.playMelody(melodies);
+  }
+
+  render() {
+    const composition = this.props.composition.map( (bot, i) => (
+        <MDBCol key={i} size="2">
+          <img src="walle_purple.png" className="robot"></img>
+        </MDBCol>
+    ));
+
+    return(
+      <div id="bottom-panel">
+        <div id="bottom-panel-label">
+          <h2>Composition</h2>
+        </div>
+        <MDBRow>
+          <MDBCol size="2">
+            <MDBIcon icon="play-circle" size="3x" id="play" onClick={this.handlePlayComposition}/>
+          </MDBCol>
+          {composition}
+        </MDBRow>
+      </div>
+    );
+  }
+}
+
+export default BottomPanel;
