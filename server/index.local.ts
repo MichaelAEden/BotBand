895ca1320e7342af8d4569be32ae5a6fcab868ee
Michael Eden <Mikey.Eden@gmail.com>
2020-06-29 22:15:36 -0400

    Use port provided by Heroku

--- server/index.local.ts
+++ server/index.local.ts
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 import app from "./app/app";
 
-const port = 9000;
+// PORT env variable is provided by Heroku
+const port = process.env.PORT || 9000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
