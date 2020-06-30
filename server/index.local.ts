b4533e9bba6c91dbef89d0037a6af59d029cf746
Michael Eden <Mikey.Eden@gmail.com>
2020-06-29 22:20:57 -0400

    Remove index.ts, rename index.local.ts to index.ts

--- server/index.local.ts
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-import app from "./app/app";
-
-// PORT env variable is provided by Heroku
-const port = process.env.PORT || 9000;
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
