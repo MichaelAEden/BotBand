a03c1d893be70656d046bb16acf93afe79852141
Michael Eden <Mikey.Eden@gmail.com>
2020-06-30 01:01:20 -0400

    Fix selectRandomWeighted returning array of undefined elements

--- server/utils/Utils.ts
+++ server/utils/Utils.ts
@@ -7,6 +7,11 @@ export const selectRandomWeighted = (items, weights, n) => {
   if (items.length !== weights.length)
     throw new Error("Items and weights must be same length!");
 
+  // If weights are all 0, set all values to 1
+  // Otherwise, array of undefined elements is returned
+  if (!weights.some((weight) => weight !== 0))
+    weights.map((weight) => weight + 1);
+
   let sum = 0;
   let segments = new Array();
   for (let i = 0; i < weights.length; i++) {
