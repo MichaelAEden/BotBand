59b4a84ef9baf6ef474ea67fceebec4a3489af87
Michael Eden <Mikey.Eden@gmail.com>
2020-07-03 01:17:11 -0400

    Fix invalid reference

--- server/src/rules/OctiveRule.ts
+++ server/src/rules/OctiveRule.ts
@@ -4,13 +4,12 @@ import { Note } from "../models/Note";
 
 export class OctiveRule extends Rule {
   apply(index: number, changeSet: Array<Note>, melody: Melody): Array<Note> {
-    
     if (index !== 0) {
-        let prevNode: Note = melody[index - 1];
-        let octiveUp = prevNode.increment(8);
-        let octiveDown = prevNode.increment(-8);
+      let prevNode: Note = melody.notes[index - 1];
+      let octiveUp = prevNode.increment(8);
+      let octiveDown = prevNode.increment(-8);
 
-        changeSet = changeSet.filter(n => n.compare(octiveUp) <= 0 && n.compare(octiveDown) >= 0);
+      changeSet = changeSet.filter((n) => n.compare(octiveUp) <= 0 && n.compare(octiveDown) >= 0);
     }
 
     return changeSet;
