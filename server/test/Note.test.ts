f0a2d1c140583eacf1fcd78a9dfb38c7a2f6b476
Michael Eden <Mikey.Eden@gmail.com>
2020-07-21 17:57:55 -0400

    Update tests

--- server/test/Note.test.ts
+++ server/test/Note.test.ts
@@ -4,12 +4,16 @@ test("parse code from note", () => {
   expect(Note.fromString("A1").code).toBe(1); // Starts at 1
   expect(Note.fromString("B1").code).toBe(2);
   expect(Note.fromString("C2").code).toBe(3); // Octave changes at C
-  expect(Note.fromString("D3").code).toBe(11);
+  expect(Note.fromString("D2").code).toBe(4);
+  expect(Note.fromString("G3").code).toBe(14); // Test end of octave
+  expect(Note.fromString("A3").code).toBe(15); // Test start of octave
 });
 
 test("parse note from code", () => {
   expect(Note.fromCode(1).note).toBe("A1"); // Starts at 1
   expect(Note.fromCode(2).note).toBe("B1");
   expect(Note.fromCode(3).note).toBe("C2"); // Octave changes at C
-  expect(Note.fromCode(11).note).toBe("D3");
+  expect(Note.fromCode(4).note).toBe("D2");
+  expect(Note.fromCode(14).note).toBe("G3"); // Test end of octave
+  expect(Note.fromCode(15).note).toBe("A3"); // Test start of octave
 });
