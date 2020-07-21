4be032c5aed1f479144396c49db3eaa9aac26669
Michael Eden <Mikey.Eden@gmail.com>
2020-07-20 21:23:27 -0400

    Fix error thrown in client when randomInitial=true

--- client/src/Utils/melody.js
+++ client/src/Utils/melody.js
@@ -1,27 +1,27 @@
 import Tone from "tone";
 
-export const NOTE_LOWEST = 12; // F3
-export const NOTE_HIGHEST = 27; // G5
+// TODO: enforce constraint in backend for this note range.
+export const NOTE_LOWEST = 12; // E3
+export const NOTE_HIGHEST = 31; // C5
 
 export const play = (melody) => {
   const synth = new Tone.Synth({
-    oscillator : {
-    type : "sine"
-    } ,
-    envelope : {
-    attack : 0.005 ,
-    decay : 0.1 ,
-    sustain : 0.3 ,
-    release : 1
-    }
+    oscillator: {
+      type: "sine",
+    },
+    envelope: {
+      attack: 0.005,
+      decay: 0.1,
+      sustain: 0.3,
+      release: 1,
+    },
   }).toMaster();
-  
+
   const sequence = new Tone.Sequence(
     function (time, note) {
-      try{
+      try {
         synth.triggerAttackRelease(note, "4n", time);
-      }
-      catch(err) {
+      } catch (err) {
         console.log(err.message);
       }
     },
