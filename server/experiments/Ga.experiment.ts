8c81978fba85f7be190be7ddb1cdee4b61a8ead7
Mickey Dang <mickeydang7299@gmail.com>
2020-07-24 11:49:06 -0400

    uncommented test

--- server/experiments/Ga.experiment.ts
+++ server/experiments/Ga.experiment.ts
@@ -5,18 +5,18 @@ import { getDiversityMetric } from "./Diversity"
 
 describe("GA Parameter Optimization Tests", () => {
   test("GA increases fitness function", () => {
-    // const config = GaWorker.defaultConfig();
-    // const trials = 1000;
+    const config = GaWorker.defaultConfig();
+    const trials = 1000;
 
-    // const results1 = runGaTrials({ ...config, iterations: 0 }, getAverageFitness, trials);
-    // const analysis1 = runAnalysis(results1);
-    // console.log(`Pregenerated melody set, fitnesses: ${JSON.stringify(analysis1, null, 2)}`);
+    const results1 = runGaTrials({ ...config, iterations: 0 }, getAverageFitness, trials);
+    const analysis1 = runAnalysis(results1);
+    console.log(`Pregenerated melody set, fitnesses: ${JSON.stringify(analysis1, null, 2)}`);
 
-    // const results2 = runGaTrials(config, getAverageFitness, trials);
-    // const analysis2 = runAnalysis(results2);
-    // console.log(`Default config, fitnesses:, ${JSON.stringify(analysis2, null, 2)}`);
+    const results2 = runGaTrials(config, getAverageFitness, trials);
+    const analysis2 = runAnalysis(results2);
+    console.log(`Default config, fitnesses:, ${JSON.stringify(analysis2, null, 2)}`);
 
-    // console.log(`Improvement in average fitness: ${analysis2.average - analysis1.average}`);
+    console.log(`Improvement in average fitness: ${analysis2.average - analysis1.average}`);
   });
 
   test("Optimal parameters", () => {
